46557e54a63114cd5db6c2aa7a186fd50d87dbb5
SAZUKI-SAMSUNG <98511431+SAZUKI-SAMSUNG@users.noreply.github.com>
2022-01-31 22:26:02 +0530

    Update Translation.py

--- DonLee_Robot_V2/Translation.py
+++ DonLee_Robot_V2/Translation.py
@@ -1,9 +1,1 @@
-# (c) [Muhammed] @PR0FESS0R-99
-# (s) @Mo_Tech_YT , @Mo_Tech_Group, @MT_Botz
-# Copyright permission under MIT License
-# All rights reserved by PR0FESS0R-99
-# License -> https://github.com/SAZUKI-SAMSUNG/DonLee-Robot-V2
-
-    START_TEXT = """
-👋𝖧𝖾𝗒  {}, Iam <a href="t.me/SAZUKI_SAMSUNGbot">➪𝚂𝙰𝚉𝚄𝙺𝙸 𝚂𝙰𝙼𝚂𝚄𝙽𝙶 ᵈᵒⁿ✞︎</a>
-𝖬𝖺𝗄𝖾 𝗆𝖾 𝖺𝗇 𝖺𝖽𝗆𝗂𝗇 𝖿𝗈𝗋 𝗒𝗈𝗎𝗋 𝗀𝗋𝗈𝗎𝗉 𝖺𝗇𝖽 𝖼𝗁𝖺𝗇𝗇𝖾𝗅 𝗍𝗁𝖾𝗇 𝖼𝗈𝗇𝗇𝖾𝖼𝗍 𝗆𝖾....
+Hu {}
