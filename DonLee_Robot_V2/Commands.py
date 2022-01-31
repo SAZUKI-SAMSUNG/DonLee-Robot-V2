82816b1709242bedf4ecc40111b5b585aa2ae086
SAZUKI-SAMSUNG <98511431+SAZUKI-SAMSUNG@users.noreply.github.com>
2022-01-31 22:39:30 +0530

    Update Commands.py

--- DonLee_Robot_V2/Commands.py
+++ DonLee_Robot_V2/Commands.py
@@ -69,7 +69,7 @@ async def start(bot: DonLee_Robot_V2, msg: Import.Msg):
     ]]
     await msg.reply_photo(
     photo=random.choice(Config.PHOTO),
-    caption=Text.START_TEXT.format(msg.from_user.mention, Config.DEV_ID),
+    caption=Text.START_TEXT.format(msg.from_user.mention
     reply_markup=Import.Markup(START_BUTTON))
 
 
