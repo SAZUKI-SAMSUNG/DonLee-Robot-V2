020193fb9762febe77b6ff1899db4eaa7b1b05fe
SAZUKI-SAMSUNG <98511431+SAZUKI-SAMSUNG@users.noreply.github.com>
2022-02-01 11:05:51 +0530

    Update Group.py

--- DonLee_Robot_V2/Group.py
+++ DonLee_Robot_V2/Group.py
@@ -457,7 +457,6 @@ async def auto_filter(bot, update):
             text=Text.SPELLING_TEXT.format(update.from_user.mention, the_query, the_query),
             parse_mode="html",
             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🕵️‍♂️ Search On Google 🕵️‍♂️", url=f"https://google.com/search?q={search}")")]]),
-                        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🕵️‍♂️ Search On Google 🕵️‍♂️", url=f"https://google.com/search?q={search}")")]]),
             reply_to_message_id=update.message_id
  reply_to_message_id=update.message_id
         )
